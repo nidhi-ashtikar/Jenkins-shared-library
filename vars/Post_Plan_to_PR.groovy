a424c9ec0c5294ec91c203eaa1cbc7588b9565f3
Nishworl <ashtikarnidhi19@gmail.com>
2024-08-27 14:14:21 +0530

    fix

---
+++ vars/Post_Plan_to_PR.groovy
@@ -0,0 +1,8 @@
+def call () {
+    def branchName = "${params.instance_name}-terraform-plan-branch"
+        withCredentials([usernamePassword(credentialsId: 'Github', usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_TOKEN')]) {
+            sh """
+            gh pr create --repo https://github.com/nidhi-ashtikar/infra-tf-creation.git --head ${branchName} --title "Terraform Plan" --body "\$(cat ${env.PLAN_OUTPUT_FILE})"
+            """
+        }
+}
