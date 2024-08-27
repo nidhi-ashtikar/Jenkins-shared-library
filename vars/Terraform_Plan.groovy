0a8c6f6f89ea88f9f75e535f34e96fc5d95b38e0
Nishworl <ashtikarnidhi19@gmail.com>
2024-08-27 13:51:27 +0530

    add

---
+++ vars/Terraform_Plan.groovy
@@ -0,0 +1,14 @@
+def call () {
+            withEnv([
+                "AWS_ACCESS_KEY_ID=${params.AWS_ACCESS_KEY_ID}",
+                "AWS_SECRET_ACCESS_KEY=${params.AWS_SECRET_ACCESS_KEY}"
+            ]) {
+                sh """
+                terraform plan -out=${env.PLAN_FILE} \
+                -var 'region=${params.region}' \
+                -var 'instance_name=${params.instance_name}' \
+                -var 'instance_type=${params.instance_type}' | tee ${env.PLAN_OUTPUT_FILE}
+                """
+            }
+    
+}
