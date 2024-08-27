3ef8f60c1fef7c5edaa74bb9f1813eb52b600cb2
Nishworl <ashtikarnidhi19@gmail.com>
2024-08-27 19:11:52 +0530

    new

--- vars/Terraform_Plan_vpc.groovy
+++ vars/Terraform_Plan_vpc.groovy
@@ -1,8 +1,8 @@
 def call () {
-            withEnv([
-                "AWS_ACCESS_KEY_ID=${params.AWS_ACCESS_KEY_ID}",
-                "AWS_SECRET_ACCESS_KEY=${params.AWS_SECRET_ACCESS_KEY}"
-            ]) {
+            // withEnv([
+            //     "AWS_ACCESS_KEY_ID=${params.AWS_ACCESS_KEY_ID}",
+            //     "AWS_SECRET_ACCESS_KEY=${params.AWS_SECRET_ACCESS_KEY}"
+            // ]) {
                 sh """
                 terraform plan -out=${env.PLAN_FILE} \
                 -var 'region=${params.region}' \
