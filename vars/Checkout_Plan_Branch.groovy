4e70042f5fedb98160668b4788fc55d41f9b857e
Nishworl <ashtikarnidhi19@gmail.com>
2024-08-27 20:55:27 +0530

    new

--- vars/Checkout_Plan_Branch.groovy
+++ vars/Checkout_Plan_Branch.groovy
@@ -1,5 +1,3 @@
 def call (){
-    withCredentials([usernamePassword(credentialsId: 'Github', usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_TOKEN')]) {
-                    git branch: "${params.PLAN_BRANCH}", url: 'https://github.com/nidhi-ashtikar/infra-tf-creation.git'
-                }
+            git branch: "${params.PLAN_BRANCH}", url: 'https://github.com/nidhi-ashtikar/infra-tf-creation.git'          
 }
