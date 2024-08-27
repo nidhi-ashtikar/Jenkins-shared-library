9e1854d18e01101138d88cb97a2233c0a3d88e49
Nishworl <ashtikarnidhi19@gmail.com>
2024-08-27 12:59:46 +0530

    add

--- vars/Terraform-Init.groovy
+++ vars/Terraform-Init.groovy
@@ -1,8 +1,5 @@
 def call () {
-    withEnv([
-        "AWS_ACCESS_KEY_ID=${params.AWS_ACCESS_KEY_ID}",
-        "AWS_SECRET_ACCESS_KEY=${params.AWS_SECRET_ACCESS_KEY}"
-    ]) {
+    {
         sh 'terraform init'
     }
 }
